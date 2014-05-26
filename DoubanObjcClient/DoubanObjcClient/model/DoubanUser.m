4e0fc628eabc1e6c01309d9806ac74a563a5a8d9
haoio <haolloyin@163.com>
2014-05-26 18:51:33 +0800

    add：实现 3个 User 接口

--- DoubanObjcClient/DoubanObjcClient/model/DoubanUser.m
+++ DoubanObjcClient/DoubanObjcClient/model/DoubanUser.m
@@ -44,7 +44,80 @@
     return [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
 }
 
+#pragma mark - Douban API
+
++ (DoubanUser *)get_user_withName:(NSString *)name
+{
+    DouApiClient *client     = [DouApiClient sharedInstance];
+    __block DoubanUser *user = nil;
+    
+    DouReqBlock callback = ^(NSData *data) {
+        NSError *error     = nil;
+        NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:1 error:&error];
+        user               = [MTLJSONAdapter modelOfClass:DoubanUser.class fromJSONDictionary:dict error:&error];
+    };
+    
+    NSString *url = [NSString stringWithFormat:@"v2/user/%@", name];
+    [client httpsGet:url withCompletionBlock:callback];
+    
+    return user;
+}
+
++ (DoubanUser *)current_user_withName:(NSString *)name
+{
+    DouApiClient *client     = [DouApiClient sharedInstance];
+    __block DoubanUser *user = nil;
+    
+    DouReqBlock callback = ^(NSData *data) {
+        NSError *error     = nil;
+        NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:1 error:&error];
+        user               = [MTLJSONAdapter modelOfClass:DoubanUser.class fromJSONDictionary:dict error:&error];
+    };
+    
+    NSString *url = @"v2/user/~me";
+    [client httpsGet:url withCompletionBlock:callback];
+    
+    return user;
+}
+
++ (NSArray *)search_user_withText:(NSString *)text start_id:(NSUInteger)start_id count:(NSUInteger)count
+{
+    DouApiClient *client          = [DouApiClient sharedInstance];
+    __block NSMutableArray *users = nil;
+    
+    DouReqBlock callback = ^(NSData *data) {
+        NSError *error     = nil;
+        NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:1 error:&error];
+        NSArray      *arr  = dict[@"users"];
+        
+        [arr enumerateObjectsUsingBlock:^(NSDictionary *dic, NSUInteger idx, BOOL *stop) {
+            DoubanUser *user = [MTLJSONAdapter modelOfClass:DoubanUser.class fromJSONDictionary:dic error:&error];
+            [users addObject:user];
+        }];
+    };
+    
+    NSUInteger start  = (start_id > 0) ? start_id : 0;
+    NSUInteger icount = (count > 0) ? count : 20;
+    
+    NSString *url = [NSString stringWithFormat:@"v2/user?q=%@&start=%d&count=%d", text, start, icount];
+    [client httpsGet:url withCompletionBlock:callback];
+    
+    return users;
+}
+
+
+
+
+
+
+
+
+
+
 @end
+
+
+
 
 @implementation DoubanSimpleUser
 
